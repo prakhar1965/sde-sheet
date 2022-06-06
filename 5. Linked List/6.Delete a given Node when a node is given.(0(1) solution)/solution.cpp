ddef86e76ad013837b01ed6df1ad6732e67bcb4a
prakhar1965 <prakhar125.nitt@gmail.com>
2022-06-06 18:56:18 +0530

    add more questions

---
+++ 5. Linked List/6.Delete a given Node when a node is given.(0(1) solution)/solution.cpp
@@ -0,0 +1,3 @@
+// Solution link: https://takeuforward.org/data-structure/delete-given-node-in-a-linked-list-o1-approach/
+// CodeStudio link: https://bit.ly/3GqW4kN
+// Leetcode link: https://leetcode.com/problems/delete-node-in-a-linked-list/
