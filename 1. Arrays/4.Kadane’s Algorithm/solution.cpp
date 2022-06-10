b9df17dc028d0dbed6e71784412cd62c5f8ccbac
prakhar1965 <prakhar125.nitt@gmail.com>
2022-06-10 23:01:05 +0530

    solved upto 012 sorting

--- 1. Arrays/4.Kadane’s Algorithm/solution.cpp
+++ 1. Arrays/4.Kadane’s Algorithm/solution.cpp
@@ -1,3 +1,26 @@
 // Solution link: https://takeuforward.org/data-structure/kadanes-algorithm-maximum-subarray-sum-in-an-array/
 // CodeStudio link: https://bit.ly/3HZltTa
 // Leetcode link: https://leetcode.com/problems/maximum-subarray/
+
+#include <bits/stdc++.h> 
+using namespace std;
+long long maxSubarraySum(int arr[], int n)
+{
+    long long globalMaxSum = 0, maxSumAtPreviousI = INT_MIN;
+
+    for(int i=0; i<n; i++)
+    {
+        long long maxSumAtI = max(maxSumAtPreviousI + arr[i], (long long)arr[i]);
+        globalMaxSum = max(globalMaxSum, maxSumAtI);
+        maxSumAtPreviousI = maxSumAtI;
+    }
+
+    return globalMaxSum;
+}
+
+// int main()
+// {
+//     int arr[] = {1 ,2 ,7, -4, 3, 2, -10, 9 ,1};
+//     int n = 9;
+//     cout<<maxSubarraySum(arr, 9);
+// }
--- 1. Arrays/5.Sort an array of 0’s 1’s 2’s/solution.cpp
+++ 1. Arrays/5.Sort an array of 0’s 1’s 2’s/solution.cpp
@@ -1,3 +1,29 @@
 // Solution link: https://takeuforward.org/data-structure/sort-an-array-of-0s-1s-and-2s/
 // CodeStudio link: https://bit.ly/3tlM60B
 // Leetcode link: https://leetcode.com/problems/sort-colors/
+
+#include <bits/stdc++.h> 
+using namespace std;
+void sort012(int *arr, int n)
+{
+   int ptr1 = 0, ptr2 = 0, ptr3 = n-1;
+
+   while(ptr2 <= ptr3)
+   {
+       switch (arr[ptr2])
+       {
+       case 0:
+           swap(arr[ptr2],arr[ptr1]);
+           ptr1++;
+           ptr2++;
+           break;
+       case 1:
+            ptr2++;
+            break;
+       case 2:
+           swap(arr[ptr3],arr[ptr2]);
+           ptr3--;
+       }
+   }
+   
+}
