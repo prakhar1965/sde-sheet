7e42297c5f83c30280fd72f36dae87bdc9dce444
prakhar1965 <prakhar125.nitt@gmail.com>
2022-06-19 00:43:42 +0530

    up to 6.3 reverse ll

--- 6. Linked List Part-II/2.Detect a cycle in Linked List/solution.cpp
+++ 6. Linked List Part-II/2.Detect a cycle in Linked List/solution.cpp
@@ -1,3 +1,38 @@
 // Solution link: https://takeuforward.org/data-structure/detect-a-cycle-in-a-linked-list/
 // CodeStudio link: https://bit.ly/3I2fmxh
 // Leetcode link: https://leetcode.com/problems/linked-list-cycle/
+
+#include <bits/stdc++.h> 
+/****************************************************************
+
+    Following is the class structure of the Node class:
+
+        class Node
+        {
+        public:
+	        int data;
+	        Node *next;
+	        Node(int data)
+	        {
+		        this->data = data;
+		        this->next = NULL;
+	        }
+        };
+
+*****************************************************************/
+
+bool detectCycle(Node *head)
+{
+    Node* slow = head, *fast = head;
+
+    while(fast->next && fast->next->next)
+    {
+        slow = slow->next;
+        fast = fast->next->next;
+        if(slow == fast) 
+        {
+            return true;
+        }
+    }
+    return false;
+}
--- 6. Linked List Part-II/3.Reverse a LinkedList in groups of size k/solution.cpp
+++ 6. Linked List Part-II/3.Reverse a LinkedList in groups of size k/solution.cpp
@@ -1,3 +1,60 @@
 // Solution link: https://takeuforward.org/data-structure/reverse-linked-list-in-groups-of-size-k/
 // CodeStudio link: https://bit.ly/3Gsfk1u
 // Leetcode link: https://leetcode.com/problems/reverse-nodes-in-k-group/
+
+#include <bits/stdc++.h> 
+using namespace std;
+/****************************************************************
+
+    Following is the class structure of the Node class:
+
+        class Node
+        {
+        public:
+	        int data;
+	        Node *next;
+	        Node(int data)
+	        {
+		        this->data = data;
+		        this->next = NULL;
+	        }
+        };
+
+*****************************************************************/
+
+int lenOfLinkedList(Node* head) {
+    int cnt = 0;
+    while(head){
+        head = head->next;
+        cnt++;
+    }
+    return cnt;
+}
+Node *getListAfterReverseOperation(Node *head, int n, int b[]){
+    Node * prevHead = new Node(-1);
+    int len = lenOfLinkedList(head);
+    prevHead->next = head;
+    Node* prev = prevHead, *curr, *nex;
+    int i = 0;
+    while(len > 0) {
+        curr = prev->next;
+        nex = curr->next;
+        int l = min(b[i], len);
+        for(int j=1; j<l; j++) {
+            curr->next = nex->next;
+            nex->next = prev->next;
+            prev->next = nex;
+            nex = curr->next;
+        }
+        if(l>0) prev = curr;
+        len -= b[i];
+        i++;
+        if(i==n) break;
+    }
+
+  
+    return prevHead->next;
+}
+
+
+
